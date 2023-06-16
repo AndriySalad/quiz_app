291ea8d53975d1222d9e7687636540dbdf61ea9f
AndruXxX <saldj.andre@gmail.com>
2023-06-17 01:02:21 +0300

    no message

--- quiz_app_frontend/src/components/form/index.js
+++ quiz_app_frontend/src/components/form/index.js
@@ -1,20 +1,56 @@
-
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import API from "../../utils/axios";
+import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
-
 const RegisterForm = () => {
-    return(
+    const navigate = useNavigate();
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required("Ім'я є обов'язковим полем"),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+        name: "",
+        },
+        validationSchema,
+        onSubmit: async (values) => {
+        await API.post("app/quiz/answerer", values)
+        .then((resp) => {
+            localStorage.setItem("user", resp.data);
+            navigate("/quiz")
+        });
+        },
+    });
+
+    const hasError = (field) =>
+        formik.touched[field] && formik.errors[field] ? "error" : "";
+
+    return (
         <div className="body">
-            <div className="container">
-                <h1>Введіть ім'я</h1>
-                <form>
-                    <label>Ім'я:</label>
-                    <input type="text" id="name" name="name" placeholder="Введіть ваше ім'я" required/>
-                    <button type="submit">Почати</button>
-                </form>
-            </div>
+        <div className="container">
+            <h1>Введіть ім'я</h1>
+            <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="name">Ім'я:</label>
+            <input
+                type="text"
+                id="name"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={hasError("name")}
+                placeholder="Введіть ваше ім'я"
+            />
+            {formik.touched.name && formik.errors.name ? (
+                <div className="error">{formik.errors.name}</div>
+            ) : null}
+            <button type="submit">Почати</button>
+            </form>
         </div>
-    )
-}
+        </div>
+    );
+};
 
 export default RegisterForm;
--- quiz_app_frontend/src/components/rating/index.js
+++ quiz_app_frontend/src/components/rating/index.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../utils/axios";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Rating = () => {
+
+    const [rating, setRating] = useState([]);
+
+    const getRating = async () => {
+        await API.get("app/quiz/answerers")
+        .then((resp) => {
+            setRating(resp.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+        getRating();
+    }, [])
+
     return(
         <div className="body">
             <div className="container">
                 <h1>Рейтинг користувачів</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Місце</th>
-                            <th>Ім'я</th>
-                            <th>Бал</th>
-                            <th>Дата</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Іван</td>
-                            <td>13</td>
-                            <td>2023-06-10</td>
-                        </tr>
-                    </tbody>
-                </table>
+                {
+                    rating.length === 0 ? <h3>Wait</h3> : <table>
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th>Місце</th>
+                                                                        <th>Ім'я</th>
+                                                                        <th>Бал</th>
+                                                                        <th>Дата</th>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    {rating.map((answerer, index) => (
+                                                                        <tr key={index}>
+                                                                            <td>{index + 1}</td>
+                                                                            <td>{answerer.name}</td>
+                                                                            <td>{answerer.total_score}</td>
+                                                                            <td>{answerer.creation_date[0]}-{answerer.creation_date[1]}-{answerer.creation_date[2]}</td>
+                                                                        </tr>
+                                                                    ))}
+                                                                </tbody>
+                                                            </table>
+                }
                 <Link className="start-testing-button" to="/register">Розпочати тестування</Link>
             </div>
         </div>
